2a2fe2a3de02d53065dfab31f0d7bd4062027929
eunicheng <eunicec1@andrew.cmu.edu>
2018-08-03 18:29:51 -0400

    add date

--- app/src/main/java/com/example/eunijane/dontforgetyourkeys/MainActivity.java
+++ app/src/main/java/com/example/eunijane/dontforgetyourkeys/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.eunijane.dontforgetyourkeys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainlayout);
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
+
+        TextView text_date = findViewById(R.id.text_date);
+        text_date.setText(currentDate);
     }
 }
